8315ed22156cbb5dce10b4926bfb3abb88ec7235
Zac Willmington <zac.willmington@gmail.com>
2018-07-05 16:45:30 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,6 +16,7 @@ class StudentsController < ApplicationController
       @student.first_name = params[:first_name]
       @student.last_name = params[:last_name]
       @student.save
+      redirect_to student_path(@student)
   end
 
 end
